83e0fcb23a47b945c253f83c8c1bbf1eb5b588bb
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-08 15:43:44 -0700

    trim source code to just get all

--- src/data/cSourceCode.js
+++ src/data/cSourceCode.js
@@ -1,5 +1,5 @@
 export default returnSource = (objName) => {
-  
+
   return
   `
   namespace NationalParksAPI.Controllers
@@ -25,66 +25,6 @@ export default returnSource = (objName) => {
         query = query.Where(entry => entry.Region.Contains(region));
       }
       return await query.ToListAsync();
-    }
-    [HttpPost]
-    public async Task<ActionResult<State>> Post(State state)
-    {
-      _db.States.Add(state);
-      await _db.SaveChangesAsync();
-      return CreatedAtAction(nameof(GetState), new {id = state.StateId}, state);
-    }
-    [HttpGet("{id}")]
-    public async Task<ActionResult<State>> GetState(int id)
-    {
-      State state = await _db.States.FindAsync(id);
-      if (state == null)
-      {
-        return NotFound();
-      }
-      return state;
-    }
-    [HttpPut("{id}")]
-    public async Task<ActionResult> Put(int id, State state)
-    {
-      if (id != state.StateId)
-      {
-        return NotFound();
-      }
-
-      _db.Entry(state).State = EntityState.Modified;
-
-      try
-      {
-        await _db.SaveChangesAsync();
-      }
-      catch (DbUpdateConcurrencyException)
-      {
-        if (!StateExists(id))
-        {
-          return NotFound();
-        }
-        else
-        {
-          throw;
-        }
-      }
-      return NoContent();
-    }
-    private bool StateExists(int id)
-    {
-      return _db.States.Any(entry => entry.StateId == id);
-    }
-    [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteMessage(int id)
-    {
-      State state = await _db.States.FindAsync(id);
-      if (state == null)
-      {
-        return NotFound();
-      }
-      _db.States.Remove(state);
-      await _db.SaveChangesAsync();
-      return NoContent();
     }
   }
 }
