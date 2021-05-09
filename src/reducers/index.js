9add9f867b5646f7401d2b55812c6e45e5664978
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-08 18:34:10 -0700

    add crud reducer to rootreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
+import crudReducer from './crud-list-reducer';
 
 const rootReducer = combineReducers({
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  crudReducer: crudReducer
 });
 
 export default rootReducer;
