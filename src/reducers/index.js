b8914d1ca953f890e7adf4194f3da97d2826595e
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-08 18:55:37 -0700

    add toggle new reducer to index and adjust naming

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
-import crudReducer from './crud-list-reducer';
+import crudListReducer from './crud-list-reducer';
+import newToggleRecuder from "./new-visible-reducer";
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
-  crudReducer: crudReducer
+  crudList: crudListReducer,
+  toggleNew: newToggleRecuder 
 });
 
 export default rootReducer;
