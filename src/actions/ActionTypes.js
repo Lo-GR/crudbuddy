019e9eab1be7a6449daf7aeca01a9c3f400a41ee
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-14 15:31:24 -0700

    remove reducers and leftover code from redux state

--- src/actions/ActionTypes.js
+++
@@ -1,2 +0,0 @@
-export const ADD_CRUD = "ADD_CRUD";
-export const TOGGLE_NEW = "TOGGLE_NEW";
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
-// import crudListReducer from './crud-list-reducer';
-// import newToggleRecuder from "./new-visible-reducer";
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer
-  // crudList: crudListReducer,
-  // toggleNew: newToggleRecuder 
 });
 
 export default rootReducer;
