de5a49c56b5ad8e7bc2a880d58540c0369f12671
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-14 14:35:30 -0700

    add hiding List and Details to Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import List from './List';
+import Details from "./Details";
+import { Context } from "../context/app-context";
 
 function Content(){
+  const {toggleList} = useContext(Context)
   return (
     <>
-      <List />
+      {toggleList ? <List /> : <Details />}
     </>
   )
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import returnSource from '../data/cSourceCode';
+
 
 function List(){
-  const testProj = "ParkAPI";
-  const testObj ="State"
-  const textToDisplay = returnSource(testProj, testObj);
+
   return (
     <>
-      <pre>
-        <code>
-            {textToDisplay}
-        </code>
-      </pre> 
+      
     </>
   )
 }
