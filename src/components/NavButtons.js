444ad1da55aaff3a2528e799ba96e0a81001f5ef
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-14 14:28:25 -0700

    add state for toggling list

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -4,9 +4,11 @@ import { Context } from "../context/app-context";
 
 function NavButtons(){
   const {toggleNew, setToggleNew} = useContext(Context);
+  const {cruds} = useContext(Context);
   return (
     <>
       <button onClick={()=>{setToggleNew(!toggleNew);}}>New Form</button>
+      <button onClick={()=>{console.log(cruds);}}>test</button>
     </>
   )
 }
--- src/context/app-context.js
+++ src/context/app-context.js
@@ -5,12 +5,14 @@ const Context = React.createContext();
 function ContextProvider({children}){
   const [cruds, setCruds] = useState({});
   const [toggleNew, setToggleNew] = useState(false);
+  const [toggleList, setToggleList] = useState(true);
   return (
     <Context.Provider value={{
       cruds: cruds,
       setCruds: setCruds,
       toggleNew: toggleNew,
-      setToggleNew: setToggleNew
+      setToggleNew: setToggleNew,
+      toggleList, setToggleList
     }}>
       {children}
     </Context.Provider>
