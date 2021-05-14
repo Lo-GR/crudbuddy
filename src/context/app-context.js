be617416c5e40afcfde1e8bf1afb660ed38d6253
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-14 15:07:20 -0700

    add selectedObj to state

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -6,13 +6,17 @@ function ContextProvider({children}){
   const [cruds, setCruds] = useState({});
   const [toggleNew, setToggleNew] = useState(false);
   const [toggleList, setToggleList] = useState(true);
+  const [selectedObj, setSelectedObj] = useState("");
   return (
     <Context.Provider value={{
       cruds: cruds,
       setCruds: setCruds,
       toggleNew: toggleNew,
       setToggleNew: setToggleNew,
-      toggleList, setToggleList
+      toggleList: toggleList,
+      setToggleList: setToggleList,
+      selectedObj: selectedObj,
+      setSelectedObj: setSelectedObj
     }}>
       {children}
     </Context.Provider>
