8fe28804f8135973e01de0cf8e2a13b7310c01a8
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-17 16:05:06 -0700

    add query parameters to source code

--- src/data/cSourceCode.js
+++ src/data/cSourceCode.js
@@ -1,8 +1,19 @@
-export const returnController = (projName, objName) => {
+export const returnController = (projName, objName, properties) => {
   const projNameSpace = projName + ".Controllers";
   const projContext = projName + "Context";
   const objController = objName + "sController";
-  
+  let queryProperties = ``;
+  let argumentProperties = ``;
+  for (let i = 0; i < properties.length; i++){
+    if(properties[i] !== "-1"){
+      queryProperties = queryProperties + `
+      if (name != null)
+      {
+        query = query.Where(entry => entry.${properties[i]}.Contains(name));
+      }`
+      argumentProperties = argumentProperties + `string ${properties[i].toLowerCase()}, `
+    }
+  }
   return `
 namespace ${projNameSpace}
 {
@@ -15,74 +26,75 @@ namespace ${projNameSpace}
       _db = db;
     }
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<${objName}>>> Get()
+    public async Task<ActionResult<IEnumerable<${objName}>>> Get(${argumentProperties})
     {
       var query = _db.${objName}s.AsQueryable();
+      ${queryProperties}
       return await query.ToListAsync();
     }
-  }
-}
-[HttpPost]
-public async Task<ActionResult<${objName}>> Post(${objName} entry)
-{
-  _db.${objName}s.Add(entry);
-  await _db.SaveChangesAsync();
-  return CreatedAtAction(nameof(Get${objName}), new {id = entry.${objName}Id}, entry);
-}
-[HttpGet("{id}")]
-public async Task<ActionResult<${objName}>> Get${objName}(int id)
-{
-  ${objName} entry = await _db.${objName}s.FindAsync(id);
-  if (entry == null)
-  {
-    return NotFound();
-  }
-  return entry;
-}
-[HttpPut("{id}")]
-public async Task<ActionResult> Put(int id, ${objName} entry)
-{
-  if (id != entry.${objName}Id)
-  {
-    return NotFound();
-  }
+    [HttpPost]
+    public async Task<ActionResult<${objName}>> Post(${objName} entry)
+    {
+      _db.${objName}s.Add(entry);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(Get${objName}), new {id = entry.${objName}Id}, entry);
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<${objName}>> Get${objName}(int id)
+    {
+      ${objName} entry = await _db.${objName}s.FindAsync(id);
+      if (entry == null)
+      {
+        return NotFound();
+      }
+      return entry;
+    }
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put(int id, ${objName} entry)
+    {
+      if (id != entry.${objName}Id)
+      {
+        return NotFound();
+      }
 
-  _db.Entry(entry).State = EntityState.Modified;
+      _db.Entry(entry).State = EntityState.Modified;
 
-  try
-  {
-    await _db.SaveChangesAsync();
-  }
-  catch (DbUpdateConcurrencyException)
-  {
-    if (!${objName}Exists(id))
-    {
-      return NotFound();
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!${objName}Exists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
     }
-    else
+    private bool ${objName}Exists(int id)
     {
-      throw;
+      return _db.${objName}s.Any(entry => entry.${objName}Id == id);
+    }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete${objName}(int id)
+    {
+      ${objName} entry = await _db.${objName}s.FindAsync(id);
+      if (entry == null)
+      {
+        return NotFound();
+      }
+      _db.${objName}s.Remove(entry);
+      await _db.SaveChangesAsync();
+      return NoContent();
     }
   }
-  return NoContent();
 }
-private bool ${objName}Exists(int id)
-{
-  return _db.${objName}s.Any(entry => entry.${objName}Id == id);
-}
-[HttpDelete("{id}")]
-  public async Task<ActionResult> Delete${objName}(int id)
-  {
-    ${objName} entry = await _db.${objName}s.FindAsync(id);
-    if (entry == null)
-    {
-      return NotFound();
-    }
-    _db.${objName}s.Remove(entry);
-    await _db.SaveChangesAsync();
-    return NoContent();
-  }
-  `
+`
 }
 
 export const returnModel = (projName, objName, properties) =>{
