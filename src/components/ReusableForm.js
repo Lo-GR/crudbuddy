de5c0a193a20b1ce81d10fbcd53895eb95f807ce
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-18 23:50:35 -0700

    major refactor and completion of hiding form material

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -3,15 +3,15 @@ import React, {useState} from "react";
 
 function ReusableForm(props){
   const [formOptions, setFormOptions] = useState({
-    showProp1: false,
+    showProp1: 0,
     showBtn1: true,
-    showProp2: false,
+    showProp2: 0,
     showBtn2: false,
-    showProp3: false,
+    showProp3: 0,
     showBtn3: false,
-    showProp4: false,
+    showProp4: 0,
     showBtn4: false,
-    showProp5: false,
+    showProp5: 0,
     showBtn5: false,
   })
   return (
@@ -25,37 +25,42 @@ function ReusableForm(props){
           type='text'
           name='objName'
           placeholder='Name of object' />
-        {formOptions.showBtn1 ? <button onClick={()=>{setFormOptions({...formOptions, showProp1: true, showBtn1: false, showBtn2: true});}}>Add Properties to Object</button> : null}
-        {formOptions.showProp1 ?
-          <input
-            type='text'
-            name='property1'
-            placeholder='Name of Property in Object' 
-            defaultValue="-1"/>
-        : null}
-        {formOptions.showBtn2 ? <button onClick={()=>{setFormOptions({...formOptions, showProp2: true, showBtn2: false, showBtn3: true});}}>Add Additional Property</button> : null}
-        {formOptions.showProp2 ?
-          <input
-            type='text'
-            name='property2'
-            placeholder='Name of Property in Object' 
-            defaultValue="-1"/>
-        : null}
+        {formOptions.showBtn1 ? <button onClick={()=>{setFormOptions({...formOptions, showProp1: 1, showBtn1: false, showBtn2: true});}}>Add Properties to Object</button> : null}
         <input
+          show={formOptions.showProp1}
+          type='text'
+          name='property1'
+          placeholder='Name of Property in Object' 
+          defaultValue="-1"/>
+        {formOptions.showBtn2 ? <button onClick={()=>{setFormOptions({...formOptions, showProp2: 1, showBtn2: false, showBtn3: true});}}>Add Additional Property</button> : null}
+        <input
+          show={formOptions.showProp2}
+          type='text'
+          name='property2'
+          placeholder='Name of Property in Object' 
+          defaultValue="-1"/>
+        {formOptions.showBtn3 ? <button onClick={()=>{setFormOptions({...formOptions, showProp3: 1, showBtn3: false, showBtn4: true});}}>Add Additional Property</button> : null}
+        <input
+          show={formOptions.showProp3}
           type='text'
           name='property3'
           placeholder='Name of Property in Object' 
           defaultValue="-1"/>
+        {formOptions.showBtn4 ? <button onClick={()=>{setFormOptions({...formOptions, showProp4: 1, showBtn4: false, showBtn5: true});}}>Add Additional Property</button> : null}
         <input
+          show={formOptions.showProp4}
           type='text'
           name='property4'
           placeholder='Name of Property in Object' 
           defaultValue="-1"/>
+        {formOptions.showBtn5 ? <button onClick={()=>{setFormOptions({...formOptions, showProp5: 1, showBtn5: false});}}>Add Additional Property</button> : null}
         <input
+          show={formOptions.showProp5}
           type='text'
           name='property5'
           placeholder='Name of Property in Object' 
           defaultValue="-1"/>
+        <small>Only a max of 5 supported</small>
         {formOptions.showProp1 ?
           <input
             type="checkbox"
