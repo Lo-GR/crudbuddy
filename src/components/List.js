d35e2fabe047388d050e11134cf924614e8c8d14
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-18 22:58:19 -0700

    add no list message

--- src/components/List.js
+++ src/components/List.js
@@ -4,13 +4,14 @@ import { Context } from "../context/app-context";
 
 function List(){
   const {cruds, setSelectedObj, setAnimate} = useContext(Context);
+  const list = Object.values(cruds).map((crud)=>
+      <div className="listItem" onClick = {() => {setSelectedObj(crud.id); setAnimate(1)}} key = {crud.id} >
+        <h2> Object: {crud.objName} | Project: {crud.projName}</h2>
+      </div>
+    )
   return (
     <>
-      {Object.values(cruds).map((crud)=>
-        <div className="listItem" onClick = {() => {setSelectedObj(crud.id); setAnimate(1)}} key = {crud.id} >
-          <h2> Object: {crud.objName} | Project: {crud.projName}</h2>
-        </div>
-      )}
+      {Object.keys(cruds).length === 0 ? <h2 className="noCrud">No CRUD Created</h2> : list}
     </>
   )
 }
