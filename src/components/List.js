e219fd0ea3cdb226759b3934642266ecc34a56ea
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-18 19:21:04 -0700

    styling for daysss

--- src/components/List.js
+++ src/components/List.js
@@ -7,7 +7,7 @@ function List(){
   return (
     <>
       {Object.values(cruds).map((crud)=>
-        <div onClick = {() => setSelectedObj(crud.id)} key = {crud.id} >
+        <div className="listItem" onClick = {() => setSelectedObj(crud.id)} key = {crud.id} >
           <h2> Object: {crud.objName} | Project: {crud.projName}</h2>
         </div>
       )}
