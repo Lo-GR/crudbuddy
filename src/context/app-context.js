d6a6c899665b8f22c13eb03f3521c128fd6fecc6
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-05-20 09:52:08 -0700

    amend cruddy text for new feature

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -34,10 +34,10 @@ function ContextProvider({children}){
  ヽ{}ノ
   /  |`
   });
-  const [explain, setExplain] = useState(["Welcome to CRUDBuddy, my name is Cruddy. I will help you build your CRUD API.", "On this site, you can add an object and project in order to create a full CRUD API Controller and Model in ASP .Net. Go ahead and try it!"])
+  const [explain, setExplain] = useState(["Welcome to CRUDBuddy, my name is Cruddy. I will help you build your CRUD API.", "On this site, you can add an object and project in order to create full CRUD API Controllers and Models in ASP .Net. Go ahead and try it!"])
   const [explainText, setExplainText] = useState({
-    newForm: ["You can enter a name of your overall project and object name here. If you want, you can also add properties.","Finally, you can check whether you want query parameters for your properties. At this time, I can only add all the queries or none of them"],
-    selectCrud: ["BAM! Here is your full CRUD API. You can swap between Model and Controller.", "Simply press the text and it will highlight automatically. You can also edit this object in the edit form."],
+    newForm: ["You can enter the names of your project, object, and properties here. Don't sweat about capitalization or white space, I'll take care of that!","You can check whether you want query parameters for your properties. At this time, I can only add all the queries or none of them"],
+    selectCrud: ["BAM! Here is your full CRUD API. You can swap between Model, Model Context, and Controller.", "Simply press the text and it will highlight automatically. The Context model automatically checks other entries for the same project name and adds those objects as well."],
     backToList: ["That's all there is to it! Feel free to add additional CRUD.", "You can find this source code and the author at https://github.com/Lo-GR/crudbuddy . "],
     editForm: ["Here you can edit your currently selected object.","Be aware, that at the moment, you'll have you reenter your entire information for the object and project. If you'd rather not, simply go back"]
   })
